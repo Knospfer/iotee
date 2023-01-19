e3ab58df6058416286cb440df1fee0d3e6ddc46a
Mattia Berrueta <berrueta.mattia@gmail.com>
2023-01-19 14:25:50 +0100

    feat: splash screen

---
+++ lib/screens/splash/slpash_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(vsync: this);
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {}
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Lottie.asset(
+          'assets/splash-logo.json',
+          width: 200,
+          height: 200,
+          controller: _controller,
+          onLoaded: (localController) {
+            _controller
+              ..duration = localController.duration
+              ..forward();
+          },
+        ),
+      ),
+    );
+  }
+}
