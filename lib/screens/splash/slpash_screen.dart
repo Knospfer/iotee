ddb4d10d708976a5cb76191d0081f04e0893bce3
Mattia Berrueta <berrueta.mattia@gmail.com>
2023-01-19 16:43:26 +0100

    feat: permission handling

--- lib/screens/splash/slpash_screen.dart
+++ lib/screens/splash/slpash_screen.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:iotee/core/route.dart';
 import 'package:lottie/lottie.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -19,10 +20,30 @@ class _SplashScreenState extends State<SplashScreen>
     super.initState();
 
     _controller = AnimationController(vsync: this);
-    _controller.addStatusListener((status) {
+    _controller.addStatusListener((status) async {
       if (status != AnimationStatus.completed) return;
+
+      await checkNeededPermissions();
       context.router.replace(const HomeRoute());
     });
+  }
+
+  Future<void> checkNeededPermissions() async {
+    final bluetooth = await Permission.bluetooth.status;
+    final location = await Permission.location.status;
+    final locationWhenInUse = await Permission.locationWhenInUse.status;
+
+    if (location.isDenied) {
+      await Permission.location.request();
+    }
+
+    if (locationWhenInUse.isDenied) {
+      await Permission.locationWhenInUse.request();
+    }
+
+    if (bluetooth.isDenied) {
+      await Permission.bluetooth.request();
+    }
   }
 
   @override
